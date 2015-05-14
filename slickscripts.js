8032941c054eae275f805e24e7f95711aeab7784
Jonathan Damico <jj_damico@yahoo.com>
2015-05-13 19:54:43 -0700

    more slick

---
+++ slickscripts.js
@@ -0,0 +1,10 @@
+$(document).ready(finction() {
+    $('.iHW-Scroller').slick({
+        dots: false,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        fade: true,
+        pauseOnHover: false,
+    });
+});
